cfc22ceeb42455f3b41707650c8f1d86c1171ea3
(s) Mark Thompson <mthompson1@pm7-0665.local>
2017-05-13 13:22:02 +0100

    Updated layout for simple title cell for Tutor Create Lecture table controller, including constraints.
    Added class for Simple Title Cell, including outlet for label.

---
+++ Live Lecture Feedback/SimpleTitleCell.swift
@@ -0,0 +1,27 @@
+
+//
+//  SimpleTitleCell.swift
+//  Live Lecture Feedback
+//
+//  Created by (s) Mark Thompson on 13/05/2017.
+//  Copyright © 2017 Devtography. All rights reserved.
+//
+
+import UIKit
+
+class SimpleTitleCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
